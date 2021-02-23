34d6f0fe063b00f466e92ef5ed80cc5f1b65f603
Trainee <trainee@isoms.local>
2021-02-23 16:11:07 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  plugins: [
+  // ...
+  require('tailwindcss'),
+  require('autoprefixer'),
+  // ...
+  ]
+}
